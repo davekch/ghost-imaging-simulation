0f4d8bba50164dba83879acc8d956d2abd839e1d
davekch <dvkch@proton.me>
2024-10-07 00:29:35 +0200

    cleanup

--- ghost_imaging_simulation.py
+++ ghost_imaging_simulation.py
@@ -13,56 +13,55 @@ def get_sample_image(path) -> np.array:
 
 
 @numba.njit
-def random_mask(shape: tuple[int, int]) -> np.array:
-    return np.random.random(shape)
+def ghost_image_step(sample: np.ndarray, intensity_expectation: float) -> np.ndarray:
+    """
+    reconstruct the image after *one* intensity measurement
+    """
+    mask = np.random.random(sample.shape)
+    intensity = (sample * mask).sum()
+    return (intensity - intensity_expectation) * mask
 
 
 @numba.njit
-def overlay(sample: np.array, mask: np.array) -> np.array:
-    return sample * mask
+def ghost_image(sample: np.ndarray, n: int) -> np.ndarray:
+    """
+    reconstruct the image after n intensity measurements
+    """
+    # the expectation value for the measured intensity is the sample + a uniformly gray mask
+    intensity_expectation = (0.5 * sample).sum()
+    result = np.zeros(sample.shape)
+    for _ in range(n):
+        result += ghost_image_step(sample, intensity_expectation)
+    return result / n
 
 
-# @numba.njit
-def ghost_image(sample: np.array, n: int, liveplot = False) -> np.array:
+def ghost_image_liveplot(sample: np.ndarray, n: int) -> np.array:
     """
-    take n intensity measurements of the sample together with a random mask
-    and return the ghost image
+    same as `ghost_image` but also create a live plot of the evolving picture
     """
-    shape = sample.shape
-    n_pixels = shape[0] * shape[1]
-    intensity_expectation = overlay(sample, 0.5*np.ones(shape)).sum()  # shape + uniformly gray mask
-    # print(f"{intensity_expectation=}")
-    result = np.zeros(shape)
-    if liveplot:
-        fig, ax = plt.subplots(ncols=2)
-        ax[0].imshow(sample, cmap="Greys_r")
+    intensity_expectation = (0.5 * sample).sum()
+    result = np.zeros(sample.shape)
+    fig, ax = plt.subplots(ncols=2)
+    ax[0].imshow(sample, cmap="Greys_r")
     for i in range(n):
-        mask = random_mask(shape)
-        intensity = overlay(sample, mask).sum()
-        # print(f"{intensity=}")
-        # add the mask with the intensity to the result
-        result += (intensity - intensity_expectation) * mask
-        if liveplot and i % 1000 == 0:
+        result += ghost_image_step(sample, intensity_expectation)
+        if i % (n // 500) == 0 or i == n - 1:
             ax[1].clear()
             ax[1].imshow(result / (i+1), cmap="Greys_r")
             ax[1].set_title(i)
-            plt.pause(0.001)
-    if liveplot:
-        plt.show()
+            plt.pause(0)
+    plt.show()
     return result / n
 
 
 if __name__ == "__main__":
     sample = get_sample_image("input.bmp")
-    result = ghost_image(sample, 500000, liveplot=True)
+    n = 5_000
+    # result = ghost_image(sample, n)
+    result = ghost_image_liveplot(sample, n)
     fig, axes = plt.subplots(nrows=1, ncols=2)
     im1 = axes[0].imshow(sample, cmap="Greys_r")
     im2 = axes[1].imshow(result, cmap="Greys_r")
-    # fig.colorbar(im2)
-    # scale result to values 0 - 255
-    lowest = np.min(result)
-    highest = np.max(result)
-    # print(lowest, highest)
-    result = 255 / (highest - lowest) * (result - lowest)
-    # plt.imsave("result.bmp", result, cmap="Greys_r")
-    # plt.show()
+    plt.imsave("reconstructed.bmp", result, cmap="Greys_r")
+    plt.savefig("result.png")
+    plt.show()
